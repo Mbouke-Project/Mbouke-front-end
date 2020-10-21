5aec3d429a4fe003612de18bd28cef5c209130a4
uharston <uharston@gmail.com>
2020-10-21 12:12:00 -0500

    Add /build

---
+++ build/precache-manifest.271cc413d3230ed72e6f8dedf54c4293.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70fbd94b4a788055feb1f2ab29de724",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa7c531f8aadad3613ae",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4980b18d8cdf41662fb",
+    "url": "./static/js/2.cc1f94ef.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cc1f94ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7c531f8aadad3613ae",
+    "url": "./static/js/main.75f54e54.chunk.js"
+  },
+  {
+    "revision": "d5f5c6676ff3adecaea4",
+    "url": "./static/js/runtime-main.aecd20ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
